178806241c33cc2f34ae62333a7747465a924cf9
asher7976 <97896895+asher7976@users.noreply.github.com>
2024-07-22 18:03:44 +0530

    Add files via upload

---
+++ terraform.tfvars
@@ -0,0 +1,3 @@
+ami_value = "ami-04a81a99f5ec58529"
+instance_type = "t2.micro"
+subnet_id_value = "subnet-0ac81a3030185dcb3"
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "ami_value" {
+  description = "value for the ami"
+}
+
+variable "instance_type" {
+    description = "value for instance type"
+}
+
+variable "subnet_id_value" {
+    description = "value of the subnet"
+}
